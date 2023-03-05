18bcc18fe2322e31cc2bac43fd92e722159e6b0a
Nithya Sri <kasidu.nithya-sri@capgemini.com>
2023-03-05 21:37:02 +0530

    commited

---
+++ Model/AdminConstants.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Pharmacy_Management_System.Model
+{
+    public class AdminConstants
+    {
+        public static List<Admin> _admin = new List<Admin>()
+        {
+            new Admin(){AdminName="admin2",Contact="1234567891",EmailId="admin2@gmail.com",Password="admin2",Role="administrator" },
+            new Admin(){AdminName="admin",Contact="1234567895",EmailId="admin@gmail.com",Password="admin",Role="administrator"},
+            new Admin(){AdminName="MainAdmin",Contact="9912674321",EmailId="vtu14218@veltech.edu.in",Password="admin",Role="administrator"}
+        };
+    }
+}
---
+++ Model/Drug.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pharmacy_Management_System.Model
+{
+    public class Drug
+    {
+        [Key]
+        public int DrugId { get; set; }
+
+
+        [Column(TypeName = "varchar(25)")]
+        [Required(ErrorMessage = "DrugName cannot be empty")]
+        public string DrugName { get; set; }
+
+        
+        [Required(ErrorMessage = "Price cannot be empty")]
+        public double DrugPrice { get; set; }
+
+        
+        [Required(ErrorMessage = "Quantity should be greater than or equal to 1")]
+        public int DrugQuantity { get; set; }
+
+
+        [Required(ErrorMessage = "Please Follow the date formate (MM/DD/YYYY)")]
+        public DateTime MfdDate { get; set; }
+
+
+        [Required(ErrorMessage ="Please Follow the date formate (MM/DD/YYYY)")]
+        public DateTime ExpDate { get; set; }
+
+
+        [Required (ErrorMessage ="Supplier Id should be required")]
+        [ForeignKey("Supplier")]
+        public int SupplierId { get; set; }
+
+        public virtual Supplier supplier { get; set; }
+
+    }
+}
